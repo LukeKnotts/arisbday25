3233b932e92b402da75b54279199a3084cf6cf8b
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-12-24 11:25:06 -0700

    first commit

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+p {
+  font-size: 24px;
+}
+body {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+
+  margin-left: 10%;
+  margin-right: 10%;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <h1>Hi, Index!</h1>
+    <p>
+      Hello, Aris! I need some more sample text in here, but I dont want to use
+      lorem ipsum. But when will it wrap???? When will it warp? I mean when will
+      it rap?
+    </p>
+    <p>{{ cakes }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let cakes = ref(0);
+</script>
