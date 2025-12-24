9f76000f583837f0583260f945807601ec7e609a
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-12-24 13:24:14 -0700

    disable text selection

--- app/app.vue
+++ app/app.vue
@@ -5,9 +5,16 @@
 </template>
 
 <style>
-/* Disable doubel tap to zoom for mobile devices **/
+/* Disable doubel tap to zoom for mobile devices, and selecting text **/
 * {
   touch-action: manipulation;
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari, Chrome */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Standard syntax */
 }
 
 /* STYLES */
