814a869a8dc6f43f1f416fa6614d6038810f4eee
Blob Guy <129005518+LukeKnotts@users.noreply.github.com>
2025-12-24 13:17:32 -0700

    disabled double click (and all kinds of) zoom on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-innerWidth, initial-scale=1.0, user-scalable=no'
+        }
+      ]
+    }
+  }
 })
